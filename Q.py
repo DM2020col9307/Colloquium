a6f08fda0c7a14718d3af2a5c61ff7ac6743b0f7
nikil58 <54081422+nikil58@users.noreply.github.com>
2020-04-17 00:18:16 +0300

    Конвертация в другие типы, остаток от деления
    Та же работа, что и в других классах + переопределено деление с остатком

--- Q.py
+++ Q.py
@@ -7,7 +7,7 @@ class Q():
         if isinstance( num, N ):
             self.num = num.toZ()
         elif isinstance( num, Z):
-            self.num = Z( num.orig )
+            self.num = Z( num.toN )
         else:
             self.num = Z( num )
 
@@ -41,6 +41,33 @@ class Q():
             return True
         else:
             return False
+
+    def toN(self):
+        if (self.ifZ and (self.num//self.denum>Z(0))):
+            return N(int(str(self.num//self.denum)))
+        else:
+            raise RuntimeError("Z", str(self), "cannot be presented as N.")
+
+    def toZ(self):
+        if (self.ifZ):
+            return Z(int(str(self.num//self.denum)))
+        else:
+            raise RuntimeError("Z", str(self), "cannot be presented as N.")
+
+    def toPoly(self):
+        return poly(self)
+
+    def tryReverseOp(a, b, op):
+        crutch = {type(N(0)): 1,
+                  type(Z(0)): 2,
+                  type(Q(0)): 3,
+                  type(poly(0)): 4
+                  }
+        if type(a) != type(b):
+            if crutch[type(a)] < crutch[type(b)]:
+                eval(type(b)(a) + op + b)
+            else:
+                eval(a + op + type(a)(b))
 
     def __add__(self, other):
         if type(self) != type(other):
@@ -78,6 +105,13 @@ class Q():
         tmp = self / other
         res = tmp.num // tmp.denum
         return res
+
+    def __mod__(self,other):
+        if type(self) != type(other):
+            return tryReverseOp(self, other, '%')
+        mod=self-(self//other)
+        return mod
+
     def __lt__(self, other):
         if type(self) != type(other):
             return tryReverseOp(self, other, '<')
