73a174c7a993a046dfe31ace9aa06075a2e11c08
nikil58 <54081422+nikil58@users.noreply.github.com>
2020-04-29 01:15:03 +0300

    Последняя версия интерфейса

--- GUI.py
+++ GUI.py
@@ -5,13 +5,15 @@ from main import *
 def clear():
     """Функция очистки полей ввода и вывода"""
     entry.delete(0, END)
+    output.config(state=NORMAL)
     output.delete(1.0,END)
+    output.config(state=DISABLED)
 def btn_help():
     """Функция вывода информативного окна для помощи пользователю в пользовании программы"""
     information = '''Добро пожаловать в алгебраическую систему для вычислений.
 С помощью данной программы Вы можете выполнять любые арифметические действия в множествах N, Z, Q и Полиномов.
 После ввода команды необходимо ввести желаемое число из N,Z,Q или Полиномов в скобки. 
-Если Вы используете числа изразных множеств - они приведутся к старшему типу, если это возможно
+Если Вы используете числа из разных множеств - они приведутся к старшему типу, если это возможно
 Основные команды:
 • GCD( ) - НОД
 • LCM( ) - НОК
@@ -19,58 +21,72 @@ def btn_help():
 • reduce( ) - Сокращение дроби
 • deg( ) - Старшая степень полинома
 • lead( ) - Старший коэффициент полинома
-• factor( ) - Факторизация полинома
 Также вы можете сравнивать и использовать любые операторы сравнения. 1 - результат вернул True, 0 - False
+Ещё доступны стандартные команды:
+• Сложение (+)
+• Вычитание (-)
+• Деление (/, //, %)
+• Возведение в степень (^, **)
 '''
     messagebox.showinfo("Help", information)
 def btn_ok():
     """Функция, отвечающая за действия при нажатии на кнопку OK"""
-    output.delete(1.0,END)
     try:
+        output.config(state=NORMAL)
+        output.delete(1.0, END)
         s=output.insert(1.0,eval(parse(input_text.get())))
+        output.config(state=DISABLED)
     except Exception as e:
         messagebox.showinfo("ОШИБКА!", e) #Вывод текста ошибки во всплывающее окно
+def press_enter(event):
+    """Функция, отвечающая за действия при нажатии на кнопку OK"""
+    output.delete(1.0, END)
+    try:
+        output.config(state=NORMAL)
+        s = output.insert(1.0, eval(parse(input_text.get())))
+        output.config(state=DISABLED)
+    except Exception as e:
+        messagebox.showinfo("ОШИБКА!", e)  # Вывод текста ошибки во всплывающее окно
 
 root = Tk()
 root.title("Алгебраическая система")                     #название окна
 root.geometry("800x600+300+100")                        #размеры окна и смещение его в центр
 root.resizable(False, False) #Отменить возможнось расширения окна
-root.iconbitmap(r'D:\Programming\Python\Colloquium\icon2.ico')
+root.iconbitmap(r'.\images\icon.ico')
+image1=PhotoImage(file=".\images\icon1.gif")
+image2=PhotoImage(file=".\images\icon2.gif")
+image3=PhotoImage(file=".\images\icon3.gif")
+image4=PhotoImage(file=".\images\icon4.gif")
+image5=PhotoImage(file=".\images\icon5.gif")
+image6=PhotoImage(file=".\images\icon6.gif")
+image7=PhotoImage(file=".\images\icon7.gif")
+image_ok=PhotoImage(file=".\images\ok.gif")
+image_help=PhotoImage(file=".\images\help.gif")
+image_clear=PhotoImage(file=".\images\clear.gif")
 input_text=StringVar()
-help_btn = Button(text = "Help",             #текст на кнопке
-             background = "#105753",     #цвет кнопки
-             foreground = "#D6EBEA",     #цвет текста
-             activebackground = "#14837D",  #цвет кнопки при нажатии
+help_btn = Button(image=image_help,
              padx = "20",               #отступ по горизонтали
              pady = "5",               #отступ по вертикали
              font = "16",                #высота шрифта
              command = btn_help             #комманда, выполняемая кнопкой
              )
 
-clear_btn = Button(text = "Clear",
-             background = "#105753",     #цвет кнопки
-             foreground = "#D6EBEA",     #цвет текста
-             activebackground = "#14837D",  #цвет кнопки при нажатии
+clear_btn = Button(image=image_clear,
              padx = "20",               #отступ по горизонтали
              pady = "5",               #отступ по вертикали
              font = "16",                #высота шрифта
              command = clear          #комманда, выполняемая кнопкой
             )
 
-ok_btn = Button(text = "OK",
-             background = "#105753",     #цвет кнопки
-             foreground = "#D6EBEA",     #цвет текста
-             activebackground = "#14837D",  #цвет кнопки при нажатии
-             height=1,
-             width=6,
+ok_btn = Button(image=image_ok,
              padx = "20",               #отступ по горизонтали
              pady = "5",               #отступ по вертикали
              font = "16",                #высота шрифта
              command = btn_ok       #комманда, выполняемая кнопкой
             )
-ok_btn.place(x=500,y=545)
-help_btn.place(x = 710, y = 545)        #позиция кнопоки
-clear_btn.place(x = 610, y = 545)       #задается координатами левого верхнего угла
+ok_btn.place(x=500,y=545, height=40, width=90)
+help_btn.place(x = 700, y = 545, height=40, width=90 )        #позиция кнопоки
+clear_btn.place(x = 600, y = 545, height=40, width=90)       #задается координатами левого верхнего угла
 
 label = Label(text = "Введите алгебраическое выражение",   #подпись(что делать)
               foreground = "#105753",               #цвет подписи
@@ -91,43 +107,39 @@ out_label = Label(text = "Результат",        #вывод програм
                )
 out_label.place(x = 110, y = 70)
 output = Text()
+output.config(state=DISABLED)
 output.place(height = 100, width = 480, x = 120, y = 115)        #координаты поля
+entry.bind('<Return>', press_enter)
 
-btn1 = Button(text = "GCD", background = "#105753", foreground = "#D6EBEA", activebackground = "#14837D",
-              height = 1, width = 13,
+btn1 = Button(
+             height = 20, width = 95, compound=TOP, image=image1,
               command = lambda x='GCD()': (entry.insert(entry.index(INSERT), x), entry.icursor(entry.index(INSERT)-1))
               ) #При нажатии на кнопку будет введен текст, хранящийся в x
 
-btn2 = Button(text = "LCM", background = "#105753", foreground = "#D6EBEA", activebackground = "#14837D",
-               height = 1, width = 13,
+btn2 = Button(height = 20, width = 95, compound=TOP, image=image2,
               command = lambda x='LCM()': (entry.insert(entry.index(INSERT), x), entry.icursor(entry.index(INSERT)-1))
               ) #При нажатии на кнопку будет введен текст, хранящийся в x
 
-btn3 = Button(text = "der", background = "#105753", foreground = "#D6EBEA", activebackground = "#14837D",
-              height = 1, width = 13,
+btn3 = Button(height = 20, width = 95, compound=TOP, image=image3,
               command = lambda x='der()': (entry.insert(entry.index(INSERT), x), entry.icursor(entry.index(INSERT)-1))
               ) #При нажатии на кнопку будет введен текст, хранящийся в x
 
 
-btn4 = Button(text = "reduce", background = "#105753", foreground = "#D6EBEA", activebackground = "#14837D",
-              height = 1, width = 13,
+btn4 = Button(height = 20, width = 95, compound=TOP, image=image4,
               command = lambda x='reduce()': (entry.insert(entry.index(INSERT), x), entry.icursor(entry.index(INSERT)-1))
               ) #При нажатии на кнопку будет введен текст, хранящийся в x
 
 
-btn5 = Button(text = "deg", background = "#105753", foreground = "#D6EBEA", activebackground = "#14837D",
-              height = 1, width = 13,
+btn5 = Button(height = 20, width = 95, compound=TOP, image=image5,
               command = lambda x='deg()': (entry.insert(entry.index(INSERT), x), entry.icursor(entry.index(INSERT)-1))
               ) #При нажатии на кнопку будет введен текст, хранящийся в x
 
 
-btn6 = Button(text = "lead", background = "#105753", foreground = "#D6EBEA", activebackground = "#14837D",
-              height = 1, width = 13,
+btn6 = Button(height = 20, width = 95, compound=TOP, image=image6,
               command = lambda x='lead()': (entry.insert(entry.index(INSERT), x), entry.icursor(entry.index(INSERT)-1))
               ) #При нажатии на кнопку будет введен текст, хранящийся в x
 
-btn7 = Button(text = "factor", background = "#105753", foreground = "#D6EBEA", activebackground = "#14837D",
-              height = 1, width = 13,
+btn7 = Button(height = 20, width = 95, compound=TOP, image=image7,
               command = lambda x='factor()': (entry.insert(entry.index(INSERT), x), entry.icursor(entry.index(INSERT)-1))
               ) #При нажатии на кнопку будет введен текст, хранящийся в x
 
